9aa14b7f6f0e4fe6ec51a145380ae02a513e247b
varunbalaji167 <varunbalaji167@gmail.com>
2024-06-02 21:57:02 +0530

    added disease panel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Panel from './Panel'
 
 const Home = () => {
   return (
-    <Navbar/>
+    <>
+     <Navbar/>
+     <Panel/>
+    </>
   )
 }
 
