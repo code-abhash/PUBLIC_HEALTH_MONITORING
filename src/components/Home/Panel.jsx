cfa70bf67d0765a4861511afc78aa1c8ace8ad84
varunbalaji167 <varunbalaji167@gmail.com>
2024-06-06 10:51:05 +0530

    added navbar to dataentry page

--- src/components/Home/Panel.jsx
+++ src/components/Home/Panel.jsx
@@ -9,15 +9,15 @@ const Panel = () => {
   return (
     <React.Fragment>
       <div className="flex flex-row justify-center  items-center mb-10 ">
-        <button className="font-bold px-6 py-4 text-xl tracking-wider hover:bg-slate-300 hover:text-black rounded-lg but_pan h-20 w-40 sm:h-14 sm:w-60">
+        <button className="font-bold bg-black text-slate-300 px-6 py-4 text-xl tracking-wider hover:bg-slate-300 hover:text-black rounded-lg but_pan h-20 w-40 sm:h-14 sm:w-60">
           Health Status
         </button>
         <div className="relative flex flex-col items-center m-2 rounded-lg">
           <button
             onClick={() => setIsOpen((prev) => !prev)}
-            className="but_pan font-bold flex items-center justify-between  px-6 py-4 text-xl tracking-wider hover:bg-slate-300 hover:text-black rounded-lg h-20 w-40 sm:h-14 sm:w-60"
+            className="but_pan bg-black text-slate-300 font-bold flex items-center justify-between  px-6 py-4 text-xl tracking-wider hover:bg-slate-300 hover:text-black rounded-lg h-20 w-40 sm:h-14 sm:w-60"
           >
-            choose Disease
+            Choose Disease
             {!isOpen ? (
               <AiOutlineCaretDown className="h-8" />
             ) : (
@@ -25,7 +25,7 @@ const Panel = () => {
             )}
           </button>
           {isOpen && (
-            <div className=" but_pan font-medium absolute top-20 flex z-10 flex-col items-start rounded-lg p-4 w-full">
+            <div className=" but_pan font-medium absolute bg-black text-slate-300 top-20 flex z-10 flex-col items-start rounded-lg p-4 w-full">
               {list.map((item, i) => (
                 <div className="p-1 w-full hover:bg-slate-300 border-b-2  hover:text-black">
                   <h3>{item.disease}</h3>
